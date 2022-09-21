40d376fb021b0f1399b1cee8dbb3f4cad3b94d6a
Łukasz Dawidowicz <lddev@outlook.com>
2022-09-21 08:04:32 +0200

    add server.ts

---
+++ server.ts
@@ -0,0 +1,43 @@
+/**
+ * Created by
+ * Łukasz Dawidowicz
+ * @2022
+ **/
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// shut down when error
+process.on("uncaughtException", (err: Error) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: `./dev.env` });
+
+// DB connection
+const DB = process.env.MONGO_DB?.replace(
+  `<PASSWORD>`,
+  process.env.DB_PASSWORD as string
+);
+
+const application = require("./src/app");
+
+mongoose
+  .connect(DB, {})
+  .then(() => {})
+  .catch((err: Error) => console.log(err));
+
+const port = process.env.PORT || 8000;
+const server = application.listen(port, () => {
+  console.log(`app running on ${port}`);
+});
+
+process.on("unhandledRejection", (err: Error) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by
+ * Łukasz Dawidowicz
+ * @2022
+ **/
+
+const express = require("express");
+
+const app = express();
+
+module.exports = app;
